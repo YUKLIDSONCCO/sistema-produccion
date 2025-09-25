c01d924e899382f13c265de36a898b83316ff709
Luis Elias Linares Ascencio <1529870@senati.pe>
2025-09-25 11:21:07 -0500

    Update index.php
    xf

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../core/Router.php';
+require_once __DIR__ . '/../core/Session.php';
+
+$router = new Router();
+$router->handleRequest();
