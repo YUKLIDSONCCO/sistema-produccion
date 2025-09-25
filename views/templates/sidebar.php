891730b11475945652291e61377f6d8ff412e1ce
Luis Elias Linares Ascencio <1529870@senati.pe>
2025-09-25 11:30:56 -0500

    Update sidebar.php
    f

---
+++ views/templates/sidebar.php
@@ -0,0 +1,17 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar p-3">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?controller=Panel&action=dashboard">📊 Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?controller=Produccion&action=index">🏭 Producción</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?controller=Usuarios&action=index">👥 Usuarios</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-danger" href="index.php?controller=Auth&action=logout">🚪 Cerrar sesión</a>
+        </li>
+    </ul>
+</nav>
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-3">
